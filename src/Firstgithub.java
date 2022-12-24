c8c5d7c6c132e37146afc85a16c90cd368d146bd
ShantanuMahalle <shantanumahalle@gmail.com>
2022-12-24 22:31:00 +0530

    Collection program

---
+++ src/Firstgithub.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public class Firstgithub {
+
+	public static void main(String[] args) {
+ArrayList list = new ArrayList();
+list.add("A");
+list.add("B");
+list.add("C");
+list.add("D");
+System.out.println(list);
+	}
+
+}
---
+++ src/Githublinkedlist.java
@@ -0,0 +1,13 @@
+import java.util.LinkedList;
+
+public class Githublinkedlist {
+public static void main(String[] args) {
+	LinkedList<Integer> list = new LinkedList<Integer>();
+	list.add(1);
+	list.add(2);
+	list.add(3);
+	list.add(4);
+	System.out.println(list);
+}
+
+}
